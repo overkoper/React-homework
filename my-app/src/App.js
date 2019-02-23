618c5112db4f4ba075942734855003341079eda4
Your Name <you@example.com>
2019-02-23 08:15:39 +0100

    added table of contents

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    people: [
+      {
+        id: 1,
+        name: "John",
+        surname: "Doe",
+        phone: "123-456-789",
+        isFavorite: false
+      },
+      {
+        id: 2,
+        name: "Steve",
+        surname: "Stevens",
+        phone: "987-654-321",
+        isFavorite: true
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Surname</th>
+                  <th>Phone</th>
+                </tr>
+              </thead>
+              <tbody>
+            {this.state.people.map(person => (
+              <tr key={person.id}>
+              <td>{person.name}</td> 
+              <td>{person.surname}</td>
+              <td>{person.phone}</td>
+              </tr>
+            ))}
+            </tbody>
+            </table>
         </header>
       </div>
     );
